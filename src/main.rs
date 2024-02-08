c8bc143645ab7199bd2af5f223ba340d32283900
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2024-02-08 16:11:05 +0200

    cargo run 101

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let arg0 = &args[0];
+    let arg1 = &args[1];
+
+    println!("The first argument is : {}", arg0);
+    println!("The second argument is : {}", arg1);
 }
