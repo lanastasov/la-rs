3c7eee94030c3d830c77eb41fc5e212cf5c8a5f4
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2024-02-09 14:51:05 +0200

    demo - cargo run 101 - added module in src subfolder

---
+++ src/demo/demo.rs
@@ -0,0 +1,3 @@
+pub fn log_mod() {
+    println!("my_module demo is imported and ready to go");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod demo {
+    pub mod demo;
+}
+
 use std::env;
 
 fn main() {
@@ -5,6 +9,10 @@ fn main() {
     let arg0 = &args[0];
     let arg1 = &args[1];
 
+    // 0.
     println!("The first argument is : {}", arg0);
     println!("The second argument is : {}", arg1);
+
+    // 1.
+    demo::demo::log_mod();
 }
