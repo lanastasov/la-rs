d3b1b320443a52bb9cf560567378d642c0235bf6
Lubomir Anastasov <lubomir.anastasov@gmail.com>
2024-02-11 00:37:37 +0200

    CodeLLDB VScode extension for debugging

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let arg0 = &args[0];
-    let arg1 = &args[1];
+    // let arg1 = &args[1];
 
     // 0.
     println!("The first argument is : {}", arg0);
-    println!("The second argument is : {}", arg1);
+    // println!("The second argument is : {}", arg1);
 
     // 1.
     demo::demo::log_mod();
